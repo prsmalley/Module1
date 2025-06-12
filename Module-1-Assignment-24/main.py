f966a07710fbafc470ab9a88f379e274e83272f4
FluffyBorkenz <prsmalley@utexas.edu>
2025-06-12 09:52:56 -0500

    Add files via upload

---
+++ Module-1-Assignment-24/main.py
@@ -0,0 +1,76 @@
+#Name:
+#Assignment 01
+''' 
+####Task 01: 
+A) Print your name, age, and favorite show
+B) Create three variables and print them. 
+C) Print a sentence containing at least one of your variables.
+
+Example: print('My name is ', name)  
+'''
+
+print('Task 01')
+#write your code on the lines 14-21
+name = 'Mr. Smalley'
+age = 100
+print('My name is', name)
+
+
+
+
+
+
+
+
+
+'''
+#####Task 02: Create a variable for any dollar amount then create another variable for a percent discount in decimal form (10% => 0.10)
+Use the variables to determine the percentage of the amount.
+'''
+
+print('\nTask 02')
+#complete the code on lines 31-34
+amount = 
+percent = 
+amount_off = 
+print("The amount off is:", amount_off)
+
+'''Optional Challenge: 
+Suppose amount_off represents an amount saved on a purchase. Show the final price after savings.
+'''
+
+print('Optional Challenge:')
+#complete the code on line 42
+amount_paid = 
+print("The amount paid is:", amount_paid)
+'''
+
+
+Task03: 
+Define two sides of a rectangle and calculate the area of the rectangle. 
+'''
+
+print('\nTask 03')
+#complete the statements on lines 53-55
+width = 
+length = 
+area = 
+print("The area is:", area)
+
+'''Optional Challenge: Use the same variables to determine the perimeter of the rectangle
+'''
+
+print('Optional Challenge:')
+#finish the statement on line 62
+perimeter = 
+print("The perimeter is:", perimeter)
+'''
+
+
+####Task 04:
+Define an integer variable and print what the remainder is when divided by 3.
+Challenge (hard): Define an integer variable and print how many times it can be divided by 3.
+'''
+
+print('\nTask 04')
+#write your code below
